b7d2143a7ff62c75bca59df876e6f5a765b8569d
allianceprojects <71387458+allianceprojects@users.noreply.github.com>
2021-03-24 08:59:21 +0700

    Add files via upload

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -91,7 +91,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN") or None
 
 # Custom (forked) repo URL and BRANCH for updater.
 UPSTREAM_REPO_URL = (os.environ.get("UPSTREAM_REPO_URL")
-                     or "https://github.com/MoveAngel/One4uBot.git")
+                     or "https://github.com/AllianceProjects/Archivicore1.0.git")
 UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH") or "sql-extended"
 
 # Console verbose logging
@@ -137,13 +137,13 @@ COUNTRY = str(os.environ.get("COUNTRY") or "")
 TZ_NUMBER = int(os.environ.get("TZ_NUMBER") or 1)
 
 # Version of One4uBot
-USERBOT_VERSION = os.environ.get("USERBOT_VERSION") or "3.7"
+USERBOT_VERSION = os.environ.get("USERBOT_VERSION") or "1.0"
 
 # User Terminal alias
-USER_TERM_ALIAS = os.environ.get("USER_TERM_ALIAS") or "One4uBot"
+USER_TERM_ALIAS = os.environ.get("USER_TERM_ALIAS") or "Archivicore"
 
 # Updater alias
-UPDATER_ALIAS = os.environ.get("UPDATER_ALIAS") or "One4uBot"
+UPDATER_ALIAS = os.environ.get("UPDATER_ALIAS") or "Archivicore"
 
 # Zipfile module
 ZIP_DOWNLOAD_DIRECTORY = os.environ.get("ZIP_DOWNLOAD_DIRECTORY") or "./zips"
